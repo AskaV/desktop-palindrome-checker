69bd8c4ee35775b19cfc5a26b8e9d1898d64e8d6
AskaV <caya2321@gmail.com>
2026-04-11 09:46:01 +0200

    add a palindrome check

--- src/main/java/com/aska/palindrom/domain/PalindromeChecker.java
+++ src/main/java/com/aska/palindrom/domain/PalindromeChecker.java
@@ -1,3 +1,23 @@
 package com.aska.palindrom.domain;
 
-public class PalindromeChecker {}
+public class PalindromeChecker {
+
+    public boolean isPalindrome(String text) {
+
+        if (text == null || text.equals("")) {
+            return false;
+        }
+        int left = 0;
+        int right = text.length() - 1;
+
+        while (left < right) {
+            if (text.charAt(left) != text.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
